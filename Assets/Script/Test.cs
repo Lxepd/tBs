099e977e9736e5bcc1a515eecfff297bd329c013
wdnmd 真茄化呀 <1360603660@qq.com>
2021-12-19 16:51:18 +0800

    20211219--1651

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test
+{
+    public bool isLoop;
+    public bool isStop;
+    public bool isTimeUp;
+
+    public float nowTime;
+    public float lastTime;
+    public float totalTime;
+    public float curTime;
+
+    public int lastUpdateFrame;
+}
